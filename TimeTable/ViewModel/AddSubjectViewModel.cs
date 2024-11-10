b7a482dcb919bb346b0afdb05c417c317f05ad62
SystemX <SystemX@DESKTOP-F94OBE7>
2024-11-11 00:36:54 +0300

    Добление новых предметов, но редактирование существующих не работает

--- TimeTable/ViewModel/AddSubjectViewModel.cs
+++ TimeTable/ViewModel/AddSubjectViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,14 +9,60 @@ using System.Windows;
 
 namespace TimeTable.ViewModel
 {
-    internal class AddSubjectViewModel
+    internal class AddSubjectViewModel : INotifyPropertyChanged
     {
+        private readonly TimeTableEntities _context;
         //без гет сет ошибка привязки данных через биндинг
-        public ObservableCollection<SubjectName> Subjects { get; set; }
-        public ObservableCollection<Teacher> Teachers { get; set; }
-        public ObservableCollection<Group> Groups { get; set; }
-        public Subject CurrentSubject { get; set; }
-        private TimeTableEntities _context;
+        private ObservableCollection<SubjectName> _subjectNames;
+        public ObservableCollection<SubjectName> SubjectNames 
+        {
+            get => _subjectNames;
+            set => SetField(ref _subjectNames, value);
+        }
+        private ObservableCollection<Teacher> _teachers;
+        public ObservableCollection<Teacher> Teachers 
+        { 
+            get => _teachers;
+            set => SetField(ref _teachers, value);
+        }
+        private ObservableCollection<Group> _groups;
+        public ObservableCollection<Group> Groups 
+        { 
+            get => _groups;
+            set => SetField(ref _groups, value);
+        }
+        private Subject _newSubject;
+        public Subject NewSubject 
+        { 
+            get => _newSubject; 
+            set => SetField(ref _newSubject, value);
+        }
+        private Subject _selectedSubject;
+        public Subject SelectedSubject
+        {
+            get =>_selectedSubject;
+            set => SetField(ref _selectedSubject, value);
+        }
+        private ObservableCollection<Subject> _listSubjects;
+        public ObservableCollection<Subject> ListSubjects
+        {
+            get => _listSubjects;
+            set => SetField(ref _listSubjects, value);
+        }
+        private void SetField<T>(ref T field, T newValue)
+        {
+            if (!Equals(field, newValue))
+            {
+                field = newValue;
+                OnPropertyChanged(nameof(field));
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
 
         private RelayCommand _createNewSubjectCommand;
         public RelayCommand CreateNewSubjectCommand
@@ -27,27 +74,69 @@ namespace TimeTable.ViewModel
                 return _createNewSubjectCommand;
             }
         }
+        private RelayCommand _saveSelectedSubjectCommand;
+        public RelayCommand SaveSelectedSubjectCommand
+        {
+            get
+            {
+                if (_saveSelectedSubjectCommand == null)
+                    _saveSelectedSubjectCommand = new RelayCommand(SaveSelectedSubject);
+                return _saveSelectedSubjectCommand;
+            }
+        }
+
         public AddSubjectViewModel()
         {
             _context = App.GetContext();
             LoadData();
+            NewSubject = new Subject();
             //LstSubjectName.AddRange(_context.SubjectName.Select(s => s.name).ToList());
         }
 
-        private async void LoadData()
+        private void LoadData()
         {
-            Subjects = new ObservableCollection<SubjectName>(_context.SubjectName.ToList());
+            SubjectNames = new ObservableCollection<SubjectName>(_context.SubjectName.ToList());
             Teachers = new ObservableCollection<Teacher>(_context.Teacher.ToList());
             Groups = new ObservableCollection<Group>(_context.Group.ToList());
+            ListSubjects = new ObservableCollection<Subject>(_context.Subject.ToList());
         }
 
         private void CreateNewSubject(object obj)
         {
-            if  (CurrentSubject == null)
+            if (NewSubject.SubjectName == null || NewSubject.Group == null)
             {
                 MessageBox.Show("Не выбраны поля для создания Предмета");
+                return;
             }
-            MessageBox.Show(CurrentSubject.Teacher.ToString());
+            try 
+            {
+                _context.Subject.Add(NewSubject);
+                //_context.SaveChanges();
+                ListSubjects.Add(NewSubject);
+                MessageBox.Show("Предмет упешно создан");
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Ошибка добавления предмета");
+            }
+        }
+
+        private void SaveSelectedSubject(object obj)
+        {
+            if (SelectedSubject.SubjectName == null)
+            {
+                MessageBox.Show("Не выбрано имя");
+                return;
+            }
+            try
+            {
+                MessageBox.Show(SelectedSubject.time.ToString());
+                //_context.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Ошибка сохранения предмета");
+            }
         }
     }
 }
